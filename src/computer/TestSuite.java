a858cbd304e990a89d44aa5acb9e34e4f0453cd9
Nishit8664 <166906466+Nishit8664@users.noreply.github.com>
2024-06-03 21:14:39 +0100

    Update TestSuite.java

--- src/computer/TestSuite.java
+++ src/computer/TestSuite.java
@@ -66,7 +66,7 @@ public class TestSuite extends Utility {
 
     @Before
     public void setUp() {                       //Open browser
-        this.openBrowser(this.baseUrl);
+        openBrowser(baseUrl);
     }
 
     @Test
